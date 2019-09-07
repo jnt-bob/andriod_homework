62a4981f07f6e121291f4e2e226207b6df886ed6
jnt-bob <917842367@qq.com>
2019-09-07 18:41:15 +0800

    hello_android

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -3,12 +3,16 @@ package com.jnu.student;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView hello_android;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello_android=(TextView)findViewById(R.id.text_view_hello_world);
+        String hello=this.getString(R.string.hello_android);
+        hello_android.setText(hello);
     }
 }
